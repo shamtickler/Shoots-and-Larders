16af1e323b806184e32b0f107b542858ba5697b3
shamtickler <nathanisonline@gmail.com>
2015-08-23 19:59:38 -0400

    Started UI
    health bar working

---
+++ ShakTheLerp/Assets/Scripts/UI/Health Script.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+var health: int;
+var healthNumber : Text;
+var healthSlider : Slider;
+
+function Start () {
+var Player : GameObject;
+Player = GameObject.FindGameObjectWithTag("Player");
+health = Player.GetComponent(PlayerCharacterController).playerHealth;
+healthSlider.maxValue = health;
+}
+
+function Update () {
+var Player : GameObject;
+Player = GameObject.FindGameObjectWithTag("Player");
+health = Player.GetComponent(PlayerCharacterController).playerHealth;
+healthNumber.text = health.ToString();
+healthSlider.value = health;
+}
