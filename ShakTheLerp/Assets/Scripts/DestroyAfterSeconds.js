323b906f3d83ae25df2cfaeb6149876db2c08f5a
shamtickler <nathanisonline@gmail.com>
2015-08-23 15:04:59 -0400

    Grenade Particles
    Still working. awesome

---
+++ ShakTheLerp/Assets/Scripts/DestroyAfterSeconds.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+var seconds : float;
+private var x: float =0;
+function Start () {
+
+}
+
+function Update () {
+x += Time.deltaTime;
+if (x >= seconds){
+	Destroy(gameObject);
+	}
+}
--- ShakTheLerp/Assets/Scripts/Grenade_Basic.js
+++ ShakTheLerp/Assets/Scripts/Grenade_Basic.js
@@ -2,6 +2,7 @@
 var radius:float = 10;
 var power: float = 200;
 var fuse: float = 5.0f;
+var particles : GameObject;
 private var timePassed: float = 0;
 
 function Start () {
@@ -11,12 +12,18 @@ function Start () {
 function Update () {
 timePassed += Time.deltaTime;
 if (timePassed >= fuse){
+Instantiate(particles, transform.position, transform.rotation);
 	var explosionPos: Vector3 = transform.position;
 	var colliders: Collider[] = Physics.OverlapSphere(explosionPos, radius);
 	for (var hit: Collider in colliders) {
 		var rb: Rigidbody = hit.GetComponent.<Rigidbody>();
-		if (rb != null)
+		if (rb != null){
 			rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+			}
+		if (hit.gameObject.tag == "Enemy"){
+			var enemyObject : GameObject = hit.gameObject;
+			enemyObject.GetComponent(Basic_Enemy).ApplyDamage(power / (Vector3.Distance(transform.position, enemyObject.transform.position)*2));
+			}
 		}
 	Destroy(gameObject);
 	}
