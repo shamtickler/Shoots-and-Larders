ceffc51afab26c9688e1f3d3e7a7e48852bfb653
shamtickler <nathanisonline@gmail.com>
2015-09-07 22:24:02 -0400

    Some ui changes

--- ShakTheLerp/Assets/Scripts/UI/MouseHoverIcon.js
+++ ShakTheLerp/Assets/Scripts/UI/MouseHoverIcon.js
@@ -3,13 +3,18 @@ import UnityEngine.UI;
 var mouseHover : Texture;
 var mouseOut : Texture;
 var imageHolder : RawImage;
+var infoTextCanvas : Canvas;
+var infoText : String;
 
 
 
 public function Enter () {
 imageHolder.texture = mouseHover;
+infoTextCanvas.gameObject.GetComponent(ShopUI).ShowInfo();
+infoTextCanvas.gameObject.GetComponent(ShopUI).infoCanvasTextFunction(infoText);
 }
 
 public function Exit () {
 imageHolder.texture = mouseOut;
+infoTextCanvas.gameObject.GetComponent(ShopUI).HideInfo();
 }
--- ShakTheLerp/Assets/Scripts/UI/ShopUI.js
+++ ShakTheLerp/Assets/Scripts/UI/ShopUI.js
@@ -1,14 +1,42 @@
 ﻿#pragma strict
 private var gold : int;
 var goldText : Text;
+var infoCanvasLeft : Canvas;
+var infoCanvasRight : Canvas;
+var infoCanvasTextLeft : Text;
+var infoCanvasTextRight : Text;
 
 function Update () {
 gold = PlayerPrefs.GetInt("PlayerGold");
 goldText.text = gold.ToString();
-
+infoCanvasLeft.transform.position = Input.mousePosition;
+infoCanvasRight.transform.position = Input.mousePosition;
 }
 
 
 public function loadLevel(level : String){
 Application.LoadLevel(level);
 }
+
+public function infoCanvasTextFunction(info : String){
+infoCanvasTextLeft.text = info;
+infoCanvasTextRight.text = info;
+}
+
+public function HideInfo(){
+infoCanvasLeft.enabled = false;
+infoCanvasRight.enabled = false;
+}
+
+public function ShowInfo(){
+if (Input.mousePosition.x > (Screen.width / 2)){
+infoCanvasRight.enabled = true;
+}else{
+infoCanvasLeft.enabled = true;
+}
+}
+public function ResetSaveFile(){
+PlayerPrefs.DeleteAll();
+}
+
+
